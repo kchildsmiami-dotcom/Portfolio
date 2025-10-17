e2f147ff750a2e6e4bda0fcc67b6f6d1d34b1e7f
kchildsmiami-dotcom <kxc2879@miami.edu>
2025-10-17 17:05:08 -0400

    renamed columns

--- scripts/01_processing/data_processing.R
+++ scripts/01_processing/data_processing.R
@@ -1,5 +1,14 @@
+#Packages
 library(tidyverse)
 library(EVR628tools)
-         
+#Data         
 shark_data<-read.csv(file="~/EVR 628/Portfolio/data/raw/shark2014.csv")
 mhw_data<-data_mhw_events
+
+#Cleaning
+## Rename columns
+shark_data<-rename(shark_data,
+       standard_length=Standard.Length,
+       fork_length=Fork.Length,
+      total_length= Total.Length)
+
