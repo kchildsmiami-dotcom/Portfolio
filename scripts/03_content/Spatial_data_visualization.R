e128069efc5bea123e2eb38bbe0a3de61c240c93
kchildsmiami-dotcom <kxc2879@miami.edu>
2025-12-01 18:52:34 -0500

    create raster versions of creef and nurse objects

--- scripts/03_content/Spatial_data_visualization.R
+++ scripts/03_content/Spatial_data_visualization.R
@@ -4,14 +4,15 @@ library(tidyverse)
 library(sf)
 library(rnaturalearth)
 library(rnaturalearthdata)
+library(raster)
 library(cowplot)
 
 
 ##Load data, filter to probable habitat locations ((It has to be like this with reading the data in 2x. I don't know why.God doesn't know why. Buddha Doesn't know why.Everything breaks if it's not. Attempt to fix at your own risk.))
-nurse <- read.csv("data/raw/nurseshark.csv")
-nurse <- read.csv("data/raw/nurseshark.csv")|>filter(nurse$Overall.Probability>0.6)
-creef<- read.csv("data/raw/creefshark.csv")
-creef<- read.csv("data/raw/creefshark.csv")|>filter(creef$Overall.Probability>0.6)
+
+nurse <- read.csv("data/raw/nurseshark.csv")|>filter(Overall.Probability>0.6)
+
+creef<- read.csv("data/raw/creefshark.csv")|>filter(Overall.Probability>0.6)
 
 
 str(nurse)
@@ -45,8 +46,13 @@ p2<-ggplot(data = world) +
   geom_sf(data=nurse_sf,aes(color=Overall.Probability))+labs(color="Nurse Shark Probability",caption = "Habitat Probabilities of Caribbean Reef & Nurse Sharks, From GBIF Sightings. Sourced From AquaMaps.") +theme(plot.caption = element_text(size=5))
 
 final_map<-plot_grid(p1, p2, ncol = 1, rel_heights = c(.9, 1))
-
+final_map
 
 ggsave("results/img/final_map.png", plot = final_map)
+
+##vector -> raster
+
+nurse_raster <- raster(nurse_sf)
+creef_raster <- raster(creef_sf)
 
 
